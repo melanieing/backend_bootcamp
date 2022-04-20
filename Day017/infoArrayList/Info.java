322659bb0c22368fdfce04e30992851c7830c205
melanieing <91274798+melanieing@users.noreply.github.com>
2022-04-20 21:31:44 +0900

    Info.java (ArrayList)

---
+++ Day017/infoArrayList/Info.java
@@ -0,0 +1,51 @@
+package info_arrayList;
+
+public class Info {
+
+	private int id;
+	private String name;
+	private int age;
+	
+	public Info() {
+		
+	}
+	public Info(int id, String name, int age) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void setInfo(int id, String name, int age) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	
+	@Override
+	public String toString() {
+		return "등록번호 : " + id + ", 이름 : " + name + ", 나이 : " + age;
+	}
+	
+	
+	
+}
