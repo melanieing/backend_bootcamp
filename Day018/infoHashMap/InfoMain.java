6b903db26a375b63b7208bb5cef7cb1ddedb2011
melanieing <91274798+melanieing@users.noreply.github.com>
2022-04-22 13:04:19 +0900

    info program (ft. HashMap)

---
+++ Day018/infoHashMap/InfoMain.java
@@ -0,0 +1,85 @@
+package info_hashMap;
+
+import java.util.Scanner;
+
+public class InfoMain {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        InfoManager manager = new InfoManager();
+
+        while (true) {
+            InfoManager.showMenu();
+            System.out.print("원하시는 메뉴의 번호를 입력하세요 : ");
+            int menu = sc.nextInt();
+            switch (menu) {
+            case 0 : // 프로그램 종료
+                return;
+            case 1 : // 회원 정보 등록
+                System.out.print("등록할 회원의 수를 입력하세요 : ");
+                int infoNum = sc.nextInt();
+                for(int i = 0; i < infoNum; i++) {
+                    System.out.print("회원의 등록번호, 이름, 나이를 차례로 입력하세요 : ");
+                    manager.joinInfo(sc.nextInt(), sc.next(), sc.nextInt());
+                }
+                System.out.println("회원 정보 등록이 완료되었습니다!");
+                break;
+            case 2 : // 회원 정보 목록 확인
+                manager.showInfo();
+                break;
+            case 3 : // 회원 정보 검색
+                System.out.print("어떤 키워드로 검색하시겠습니까? (등록번호(1), 이름(2), 나이(3) 중 번호로 선택) : ");
+                int clue = sc.nextInt();
+                if (clue == 1) {
+                    System.out.print("찾으시려는 회원의 등록번호를 입력하세요 : ");
+                    int clueId = sc.nextInt();
+                    manager.printInfo(clueId);
+                } else if (clue == 2) {
+                    System.out.print("찾으시려는 회원의 이름을 입력하세요 : ");
+                    String clueName = sc.next();
+                    int nameAnswer = manager.searchByName(clueName);
+                    if (nameAnswer == -1) {
+                    	System.out.println("해당 이름을 가진 회원은 없습니다!");
+                    	break;
+                    }
+                    manager.printInfo(nameAnswer);
+                } else if (clue == 3) {
+                    System.out.print("찾으시려는 회원의 나이를 입력하세요 : ");
+                    int clueAge = sc.nextInt();
+                    int ageAnswer = manager.searchByAge(clueAge);
+                    if (ageAnswer == -1) {
+                    	System.out.println("해당 나이를 가진 회원은 없습니다!");
+                    	break;
+                    }
+                    manager.printInfo(ageAnswer);
+                } else {
+                    System.out.println("1, 2, 3 중에서 입력하세요!");
+                }
+                break;
+            case 4 : // 회원 정보 수정
+                System.out.print("정보를 수정할 회원의 등록번호를 입력하세요 : ");
+                int targetId = sc.nextInt();
+                if (manager.searchById(targetId) == null) {
+                	System.out.println("해당 등록번호를 가진 회원은 없습니다!");
+                	continue;
+                }
+            	System.out.println("새로운 정보를 이름, 나이 순으로 입력하세요 : ");
+            	manager.modifyInfo(targetId, sc.next(), sc.nextInt());
+                System.out.println("학생 정보 수정을 완료했습니다!");
+            	break;
+            case 5 : // 회원 정보 삭제
+                System.out.print("삭제할 회원의 등록번호를 입력하세요 : ");
+                int targetId2 = sc.nextInt();
+                if (!manager.deleteInfo(targetId2)) {
+                	System.out.println("해당 등록번호를 가진 회원은 없습니다!");
+                } else {
+                	System.out.println("해당 회원 정보를 삭제했습니다!");
+                }
+                break;
+            default :
+            	System.out.println("0 ~ 5 사이의 숫자를 입력하세요!");
+            }
+        }
+    }
+}
---
+++ Day018/infoHashMap/InfoManager.java
@@ -0,0 +1,124 @@
+package info_hashMap;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class InfoManager {
+
+	private int id;
+    private HashMap<Integer, Info> infos;
+    public static final int ERROR = -1;
+
+    // 기본 메뉴 구성
+    public static void showMenu() {
+        System.out.println("\n=====회원 정보 관리 프로그램=====");
+        System.out.println("1. 회원 정보 등록");
+        System.out.println("2. 회원 정보 확인");
+        System.out.println("3. 회원 정보 검색");
+        System.out.println("4. 회원 정보 수정");
+        System.out.println("5. 회원 정보 삭제");
+        System.out.println("0. 프로그램 종료");
+    }
+
+    // initializing constructor
+    public InfoManager() {
+        infos = new HashMap<>();
+    }
+
+    // 1. 회원 정보 등록
+    public void joinInfo(int id, String name, int age) {
+        this.infos.put(id, new Info(name, age));
+    }
+
+    // 2-1. 회원 정보 전체 목록 확인
+    public void showInfo() {
+        if (!infos.isEmpty()) {                
+        	System.out.println("-----회원 정보 목록-----");
+        	for (Entry<Integer, Info> entry : infos.entrySet()) {
+        		System.out.println("[ID] : " + entry.getKey() + " " + entry.getValue());
+        	}
+        } else {
+            System.out.println("등록된 회원 정보가 없습니다!");
+        }
+    }
+
+    // 2-2. 개별 회원 정보 출력
+    public void printInfo(int id) {
+    	if (!infos.containsKey(id)) {
+    		System.out.println("등록된 회원 정보가 없습니다!");
+    	} else {
+    		System.out.println("[ID] : " + id + ", 이름 : " + infos.get(id).getName() + ", 나이 : " + infos.get(id).getAge());
+    	}	
+    }
+    
+    public void printInfo(HashMap<Integer, Info> target) {
+    	Object targetKey = target.keySet().toArray()[0];
+    	printInfo((int)targetKey);
+    }
+    
+    // 3. 회원 정보 검색
+    // 3-1. 회원 등록번호로 검색
+    public HashMap<Integer, Info> searchById(int id) {
+    	Iterator<Entry<Integer, Info>> it = infos.entrySet().iterator();
+    	while (it.hasNext()) {
+    		Map.Entry<Integer, Info> entry = it.next();
+    		if (id == entry.getKey()) {
+    			return infos;
+    		}
+    	}
+    	return null;
+    }   
+
+    // 3-2. 회원 이름으로 검색
+    public int searchByName(String name) {
+    	for (Map.Entry<Integer, Info> entry : infos.entrySet()) {
+    		int targetkey = entry.getKey();
+    		String targetName = entry.getValue().getName();
+    		if (targetName.equals(name)) {
+    			return targetkey;
+    		}
+    	}
+    	return ERROR;
+    }   
+
+    // 3-3. 회원 나이로 검색
+    public int searchByAge(int age) {
+    	for (Map.Entry<Integer, Info> entry : infos.entrySet()) {
+    		int targetkey = entry.getKey();
+    		int targetAge = entry.getValue().getAge();
+    		if (targetAge == age) {
+    			return targetkey;
+    		}
+    	}
+    	return ERROR;
+    }   
+
+    // 4. 회원 정보 수정
+    public boolean modifyInfo(int id, String newName, int newAge) {
+    	if (searchById(id) == null) {
+    		return false;
+    	}
+    	Info newInfo = new Info(newName, newAge);
+    	this.searchById(id).put(id, newInfo);
+        return true;
+   }
+
+    // 5. 회원 정보 삭제
+    public boolean deleteInfo(int id) {
+    	if (searchById(id) == null) {
+    		return false;
+    	}
+    	infos.remove(id);
+    	return true;        
+    }
+
+	@Override
+	public String toString() {
+		return "InfoManager [id=" + id + ", infos=" + infos + "]";
+	}
+    
+    
+
+}
